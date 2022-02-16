c24551a1711e93a3f7c8657517e34ae6a2d41dd5
Piet Althoff <pietalthoff@googlemail.com>
2022-02-16 23:30:04 +0100

    add windi css again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,8 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-/*     buildModules: [
+    buildModules: [
         'nuxt-windicss',
-    ] */
+    ],
     css: ['@/assets/css/main.css'],
 })
